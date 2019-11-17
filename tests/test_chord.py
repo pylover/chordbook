491a49c96baab8034af4707b0d65890ec7cdef99
pylover <vahid.mardani@gmail.com>
2019-11-17 11:00:52 +0330

    add9 chords

--- tests/test_chord.py
+++ tests/test_chord.py
@@ -27,17 +27,76 @@ def test_parse_chord():
     assert str(Chord.parse('Cdim7')) == 'C-D#-F#-A'
     assert str(Chord.parse('Caug')) == 'C-E-G#'
     assert str(Chord.parse('C5')) == 'C-G'
-#    assert str(Chord.parse('C/5+')) == 'C-E-G#'
-#    assert str(Chord.parse('C/5-')) == 'C-E-F#'
-#    assert str(Chord.parse('Cadd9')) == 'C-E-G-D'
-#    assert str(Chord.parse('Cadd9-')) == 'C-E-G-C#'
-#    assert str(Chord.parse('Cadd9+')) == 'C-E-G-D#'
-#    assert str(Chord.parse('Cadd11')) == 'C-E-G-F'
-#    assert str(Chord.parse('Cadd11-')) == 'C-E-G-E'
-#    assert str(Chord.parse('Cadd11+')) == 'C-E-G-F#'
+    assert str(Chord.parse('C9')) == 'C-E-G-A#-D'
+    assert str(Chord.parse('C9-')) == 'C-E-G-A#-C#'
+    assert str(Chord.parse('C9+')) == 'C-E-G-A#-D#'
+    assert str(Chord.parse('C7add9')) == 'C-E-G-A#-D'
+    assert str(Chord.parse('C7add9-')) == 'C-E-G-A#-C#'
+    assert str(Chord.parse('C7add9+')) == 'C-E-G-A#-D#'
+    assert str(Chord.parse('Cadd9')) == 'C-E-G-D'
+    assert str(Chord.parse('Cadd9-')) == 'C-E-G-C#'
+    assert str(Chord.parse('Cadd9+')) == 'C-E-G-D#'
+    assert str(Chord.parse('Cmaj7add9+')) == 'C-E-G-B-D#'
+    assert str(Chord.parse('Cmaj7add9-')) == 'C-E-G-B-C#'
+    assert str(Chord.parse('Cadd11')) == 'C-E-G-F'
+    assert str(Chord.parse('Cadd11-')) == 'C-E-G-E'
+    assert str(Chord.parse('Cadd11+')) == 'C-E-G-F#'
+
+    assert str(Chord.parse('C/5-')) == 'C-E-F#'
+    assert str(Chord.parse('C/5+')) == 'C-E-G#'
+
 #    assert str(Chord.parse('Cadd13')) == 'C-E-G-A'
 #    assert str(Chord.parse('Cadd13-')) == 'C-E-G-G#'
 #    assert str(Chord.parse('Cadd13+')) == 'C-E-G-A#'
 #    assert str(Chord.parse('Cadd13+/5+')) == 'C-E-G#-A#'
 #
     #assert str(Chord.parse('C13/5+/9+/11-')) == 'C-E-G-A#'
+
+'''
+          0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2
+          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4
+          C   D   E F   G   A   B C   D   E F   G   A   B C
+          . . . . . . . . . . . . . . . . . . . . . . . . .
+
+C         | . . . | . . | . . . . . . . . . . . . . . . . .
+C7        | . . . | . . | . . | . . . . . . . . . . . . . .
+Cmaj7     | . . . | . . | . . . | . . . . . . . . . . . . .
+C6        | . . . | . . | . | . . . . . . . . . . . . . . .
+Cm        | . . | . . . | . . . . . . . . . . . . . . . . .
+Cm7       | . . | . . . | . . | . . . . . . . . . . . . . .
+Cm/maj7   | . . | . . . | . . . | . . . . . . . . . . . . .
+Cm6       | . . | . . . | . | . . . . . . . . . . . . . . .
+Csus2     | . | . . . . | . . . . . . . . . . . . . . . . .
+Csus4     | . . . . | . | . . . . . . . . . . . . . . . . .
+C7sus2    | . | . . . . | . . | . . . . . . . . . . . . . .
+C7sus4    | . . . . | . | . . | . . . . . . . . . . . . . .
+Cdim      | . . | . . | . . . . . . . . . . . . . . . . . .
+Cdim7     | . . | . . | . . | . . . . . . . . . . . . . . .
+Caug      | . . . | . . . | . . . . . . . . . . . . . . . .
+C5        | . . . . . . | . . . . . . . . . . . . . . . . .
+C9        | . . . | . . | . . | . . . | . . . . . . . . . .
+Cadd9     | . . . | . . | . . . . . . | . . . . . . . . . .
+
+----
+Cadd11    | . . . | . . | . . . . . . . . . | . . . . . . .
+Cadd13    | . . . | . . | . . . . . . . . . . . . . | . . .
+C/5+      | . . . | . . . | . . . . . . . . . . . . . . . .
+C/5-      | . . . | . | . . . . . . . . . . . . . . . . . .
+Cm6       | . . | . . . | . | . . . . . . . . . . . . . . .
+Cm/maj7   | . . | . . . | . . . | . . . . . . . . . . . . .
+
+Panthetonic
+Cm9       | . . | . . . | . . | . . . | . . . . . . . . . .
+C9-       | . . . | . . | . . | . . | . . . . . . . . . . .
+C9+       | . . . | . . | . . | . . . . | . . . . . . . . .
+
+Sixtual
+Cm11      | . . | . . . | . . | . . . | . . | . . . . . . .
+C11       | . . . | . . | . . | . . . | . . | . . . . . . .
+
+Seven
+Cm13      | . . | . . . | . . | . . . | . . | . . . | . . .
+C13       | . . . | . . | . . | . . . | . . | . . . | . . .
+
+'''
+
